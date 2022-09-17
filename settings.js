7a824daa41fb32526b9b6b44eeeae3284cb3b14d
aquahasibot <113094068+aquahasibot@users.noreply.github.com>
2022-09-17 13:53:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['14153260854'] //ur owner number
+global.ownername = "𝙼𝙰𝙳 𝙷𝙰𝚂𝙸𝚈𝙰" //ur owner name
+global.ytname = "YT: 𝙼𝙰𝙳 𝙷𝙰𝚂𝙸𝚈𝙰" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.location = "Sri Lanka, Awissawella" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['14153260854']
+global.ownertag = '14153260854' //ur tag number
+global.botnumber = '94779711041' //Add your Bot Number For Log
+global.botname = '𝙼𝙰𝙳 𝙷𝙰𝚂𝙸𝚈𝙰 ²⁰²² EDITION' //ur bot name
+global.ig = "https://www.facebook.com/Mafiyagaming24" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.facebook.com/Mafiyagaming24" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
-global.reactmoji = "😘" //ur menu react emoji
+global.reactmoji = "👽" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.packname = "𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 © 𝙼𝙰𝙳 𝙷𝙰𝚂𝙸𝚈𝙰 ²⁰²²" //ur sticker watermark packname
+global.author = "© 𝙼𝙰𝙳 𝙷𝙰𝚂𝙸𝚈𝙰" //ur sticker watermark author
+global.footer = "© 𝙼𝙰𝙳 𝙷𝙰𝚂𝙸𝚈𝙰 ᴇᴅɪᴛɪᴏɴ"
+global.aliveimg ="https://i.ibb.co/S35ZkCM/20220815-110819.jpg"
+global.allmenuimg ="https://i.ibb.co/S35ZkCM/20220815-110819.jpg"
+global.welcomeimg= "https://i.ibb.co/S35ZkCM/20220815-110819.jpg"
+global.gbyeimg = "https://i.ibb.co/S35ZkCM/20220815-110819.jpg"
+global.alivemsg = "*𝚈𝙾𝚄𝚁 𝙻𝙾𝚅𝙴𝙻𝚈 𝙱𝙾𝚃 𝙰𝙻𝙸𝚅𝙴 👽*"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
